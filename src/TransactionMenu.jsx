c6a18dbf7b79c2b37e7bdba9748ab7f9596ffe78
Grant Willison <GWWillisonBusiness@gmail.com>
2025-06-27 21:55:28 -0400

    6/27/25 Changes, Version 0.1.2

--- src/TransactionMenu.jsx
+++ src/TransactionMenu.jsx
@@ -1,39 +1,50 @@
 import React, { useState } from "react";
 import "./App.css"; // or wherever your styles are
 
-const TaskMenu = ({ onSubmit, onClose, show }) => {
+const TransactionMenu = ({ onSubmit, onClose, show }) => {
   const [amount, setAmount] = useState("");
-  const [type, setType] = useState("add");
+  const [transactionType, setTransactionType] = useState("add");
+  const [categoryType, setCategoryType] = useState("");
 
   const handleSubmit = () => {
     const number = parseFloat(amount);
     if (isNaN(number) || number <= 0) return;
-    onSubmit(type, number);
+    onSubmit(transactionType, number);
     setAmount("");
-    setType("add");
+    setTransactionType("add");
     onClose();
   };
 
   return (
+    //Note: In Second Select Statement I need to add the categories to it!
     <div className={`taskOverlay ${show ? "show" : ""}`} onClick={onClose}>
       <div className="taskPanel" onClick={(e) => e.stopPropagation()}>
-        <h3>{type === "add" ? "Add Money" : "Subtract Money"}</h3>
+        <h3>{transactionType === "add" ? "Add Money" : "Subtract Money"}</h3>
 
         <input
           type="number"
           value={amount}
           onChange={(e) => setAmount(e.target.value)}
-          placeholder="Enter amount"
+          placeholder="Enter Amount"
           style={{ marginBottom: "1rem", padding: "0.5rem", width: "95%" }}
         />
 
         <select
-          value={type}
-          onChange={(e) => setType(e.target.value)}
+          value={transactionType}
+          onChange={(e) => setTransactionType(e.target.value)}
           style={{ marginBottom: "1rem", padding: "0.5rem", width: "100%" }}
         >
           <option value="add">Add</option>
           <option value="subtract">Subtract</option>
+        </select>
+
+        <select
+          value={categoryType}
+          onChange={(e) => setCategoryType(e.target.value)}
+          style={{ marginBottom: "1rem", padding: "0.5rem", width: "100%" }}
+        >
+          <option value="1">Categories</option>
+          <option value="2">Categories 2</option>
         </select>
 
         <button
@@ -47,4 +58,4 @@ const TaskMenu = ({ onSubmit, onClose, show }) => {
   );
 };
 
-export default TaskMenu;
+export default TransactionMenu;
