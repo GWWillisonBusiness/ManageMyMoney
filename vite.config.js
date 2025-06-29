c8f07940543514af3e25c6e87facfd499ef52dfe
Grant Willison <GWWillisonBusiness@gmail.com>
2025-06-29 16:17:01 -0400

    safari support

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
-  }
-})
+    target: "es2015", // This is key for Safari support
+    outDir: "dist",
+  },
+});
